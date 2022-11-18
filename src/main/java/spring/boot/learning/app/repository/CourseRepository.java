fb4028d8105c5d1d0ecbcf95ad1dc9d062f131bc
hieu.nq <ngohieu1811@gmail.com>
2022-11-18 23:18:01 +0700

    thêm search theo name

--- src/main/java/spring/boot/learning/app/repository/CourseRepository.java
+++ src/main/java/spring/boot/learning/app/repository/CourseRepository.java
@@ -1,5 +1,8 @@
 package spring.boot.learning.app.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spring.boot.core.api.CoreRepository;
 import spring.boot.learning.app.dto.CourseDTO;
@@ -8,4 +11,9 @@ import spring.boot.learning.app.entity.CourseEntity;
 @Repository
 public interface CourseRepository extends CoreRepository<CourseDTO, CourseEntity> {
 
+  @Override
+  @Query("select e from CourseEntity e" +
+      " where (lower(e.name) like %:#{#dto.name}% or :#{#dto.name} is null) "
+  )
+  Page<CourseEntity> search(CourseDTO dto, Pageable pageable);
 }
